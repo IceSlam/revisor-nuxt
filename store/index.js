d56e8a8a7e2e751ea19dc839fd445f0c5d3114f7
Icelam <mainguy@mail.ru>
2020-12-10 23:37:48 +0300

    Edited paths

--- store/index.js
+++ store/index.js
@@ -6,56 +6,26 @@ Vue.use(Vuex)
 
 const store = () => new Vuex.Store({
   state: {
-    services: [],
-    news: [],
-    about: {}
+    services: []
   },
   mutations: {
     SET_SERVICES_TO_STORE: (state, services) => {
       state.services = services
-    },
-    SET_ABOUT_TO_STORE: (state, services) => {
-      state.services = services
-    },
-    SET_NEWS_TO_STORE: (state, news) => {
-      state.news = news
     }
   },
   actions: {
     GET_SERVICES_FROM_API ({ commit }) {
-      return axios('https://buhrevizor.ru/wp-json/wp/v2/posts/?categories=3&per_page=50', {
+      return axios('https://strapi-test-back.herokuapp.com/articles', {
         method: 'GET'
       })
         .then((response) => {
           commit('SET_SERVICES_TO_STORE', response.data)
-        })
-    },
-    GET_ABOUT_FROM_API ({ commit }) {
-      return axios('https://buhrevizor.ru/wp-json/wp/v2/pages/14', {
-        method: 'GET'
-      })
-        .then((response) => {
-          commit('SET_ABOUT_TO_STORE', response.data)
-        })
-    },
-    GET_NEWS_FROM_API ({ commit }) {
-      return axios('https://buhrevizor.ru/wp-json/wp/v2/posts/?categories=2&per_page=50', {
-        method: 'GET'
-      })
-        .then((response) => {
-          commit('SET_NEWS_TO_STORE', response.data)
         })
     }
   },
   getters: {
     SERVICES (state) {
       return state.services
-    },
-    ABOUT (state) {
-      return state.about
-    },
-    NEWS (state) {
-      return state.news
     }
   },
   modules: {
